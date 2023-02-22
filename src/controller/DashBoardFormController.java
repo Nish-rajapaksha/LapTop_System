12c4bd2865dcee368784e6d58ff528e18050b8b7
Nish-rajapaksha <nishrajapaksha213@gmail.com>
2023-02-22 12:53:18 +0530

    Upload project LaptopSave

--- src/controller/DashBoardFormController.java
+++ src/controller/DashBoardFormController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class DashBoardFormController {
+    public void saveLaptopOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchLaptopOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateLaptopOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteLaptopOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadAllaptopOnAction(ActionEvent actionEvent) {
+    }
 }
